24a9d87d116009903a08e10e1e1201a569b686ac
caohaianh2004 <caohaianh26@gmail.com>
2024-08-31 13:43:27 +0700

    luyen tap

---
+++ main.js
@@ -0,0 +1,69 @@
+// // // // alert("Hi js")
+// // // alert("Hello word")
+
+// // /*
+// // *o
+
+// // */
+// // var fullName = 'Cao Van Hai Anh';//khai báo tên
+// // var age = 20;//khai báo tuổi
+
+// // //gọi hàm thông báo
+// // alert(fullName);
+// // alert(age);
+
+// var fullName = 'Cao Van Hai Anh'
+
+// console.log(fullName);
+
+// confirm('Xac nhan ban du tuoi')
+
+// prompt('Xac nhan do tuoi')
+
+// setTimeout(function () {
+//     alert('thong bao')
+// },1000)
+
+// setInterval(function(){
+//     console.log('ddddddddd' + Math.random());
+
+// },1000)
+
+// var age = 20
+// var nextAge= age+1
+// console.log(age);
+
+// var a= 1;
+// a += 2;
+// console.log(a);
+
+// console.log('--Hậu tố--');
+// var number1 = 1;
+// console.log(number1++);
+// console.log(number1);
+
+// var a = 6;
+// var output = a++ + --a;
+// console.log('a: ', a);
+
+//Toán tử nối chuỗi
+// var a = 'Hải Anh';
+// var c = 'YÊU';
+// var b = 'Ánh Linh';
+
+// console.log(a + ' ' + c + ' ' + b);
+
+// var age = 10
+
+// if (age < 18) {
+//     console.log('Không đủ tuổi mua rượu');
+// } else {
+//     console.log('Đủ tuổi mua rượu');
+// }
+
+
+
+
+
+
+
